0698036db6780a906a1ef02af4c70cdc29336b44
Alan Moreno <sammp0511@gmail.com>
2022-07-12 00:18:06 -0500

    Segundo Commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/capi-examen-front-alan-moreno'),
+      dir: require('path').join(__dirname, './coverage/capi-examen-front-Alan-Moreno'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'capi_examen_front_Alan_Moreno'`, () => {
+  it(`should have as title 'capi-examen-front-Alan-Moreno'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('capi_examen_front_Alan_Moreno');
+    expect(app.title).toEqual('capi-examen-front-Alan-Moreno');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('capi_examen_front_Alan_Moreno app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('capi-examen-front-Alan-Moreno app is running!');
   });
 });
